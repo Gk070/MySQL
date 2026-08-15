8fc0338f50e412137e104af9af979c389c5da594
Gk070 <geethulkrishna@gmail.com>
2026-08-15 13:45:41 +0530

    GROUP BY & ORDER BY

---
+++ Class 3.sql
@@ -0,0 +1,14 @@
+-- GROUP BY
+
+SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age) FROM employee_demographics 
+GROUP BY gender;
+
+SELECT occupation, salary FROM employee_salary GROUP BY occupation, salary;
+
+-- ORDER BY
+
+SELECT * FROM employee_demographics ORDER BY first_name DESC;
+
+SELECT * FROM employee_demographics ORDER BY gender, age DESC;
+
+SELECT * FROM employee_demographics ORDER BY 5, 4;
